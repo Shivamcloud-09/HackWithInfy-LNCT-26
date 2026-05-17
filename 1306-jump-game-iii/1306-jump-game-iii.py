c3c8e36f1f68a425e4e9274aa2f7063a8d9eec3d
SHIVAM_GUPTA <shivamj9929@gmail.com>
2026-05-17 23:27:59 +0530

    Time: 10 ms (69.82%), Space: 38.9 MB (22.54%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        vis = set()
+        def dfs(i):
+            if i < 0 or i >= len(arr) or i in vis:
+                return False
+            if arr[i] == 0:
+                return True
+            vis.add(i)
+            return dfs(i + arr[i]) or dfs(i - arr[i])
+        return dfs(start)
